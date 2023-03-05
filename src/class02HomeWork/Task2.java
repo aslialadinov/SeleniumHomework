9f1181ef128dc1a0ff3de5232b71fe52b4b5ccb5
aslialadinov <asaladinov@gmail.com>
2023-03-04 22:41:22 -0500

    Selenium code from class

---
+++ src/class02HomeWork/Task2.java
@@ -0,0 +1,43 @@
+package class02HomeWork;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task2 {
+   /*
+   HW2
+   navigate to fb.com
+   click on create new account
+   fill up all the text-boxes
+   click on sign up button
+   close the pop-up
+   close the browser
+   just fill in text boxes or button, no need to deal with other webElements
+   also u will need a Thread.sleep after clcikng on create new account
+    */
+   public static  void main(String[] args) throws InterruptedException {
+       System.setProperty("webdriver.chrome.driver","Driver/chromedriver.exe");
+       WebDriver driver=new ChromeDriver();
+       driver.get("https://www.facebook.com/");
+       driver.manage().window().maximize();
+       Thread.sleep(3000);
+       driver.findElement(By.linkText("Create new account")).click();
+       Thread.sleep(3000);
+       driver.findElement(By.name("firstname")).sendKeys("Eva");
+       driver.findElement(By.name("lastname")).sendKeys("Swift");
+       driver.findElement(By.name("reg_email__")).sendKeys("swift@gmail.com");
+       driver.findElement(By.name("reg_email_confirmation__")).sendKeys("swift@gmail.com");
+       driver.findElement(By.name("reg_passwd__")).sendKeys("nmkj4578");
+       driver.findElement(By.name("birthday_month")).sendKeys("February");
+       driver.findElement(By.name("birthday_day")).sendKeys("4");
+       driver.findElement(By.name("birthday_year")).sendKeys("2000");
+      // driver.findElement(By.name("sex")).click();
+       //driver.findElement(By.xpath("//*[contains(text(),'Male')]")).click();
+       driver.findElement(By.name("websubmit")).click();
+     //  driver.findElement(By.xpath("//*[contains(text),'Continue')]")).click();
+     //  driver.findElement(By.linkText("Continue")).click();
+       Thread.sleep(3000);
+       driver.quit();
+   }
+}
---
+++ src/class03HomeWork/Xpath.java
@@ -0,0 +1,45 @@
+package class03HomeWork;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Xpath {
+    /*
+    use xpath to complete this
+goto http://practice.syntaxtechs.net/basic-first-form-demo.php
+Syntax - Website to practice Syntax Automation Platform
+Syntax Automation Platform has great examples like Ajax, JQuery, Bootstrap etc which are used in most of the
+web applications around the world.
+enter the message in the text box
+click on show message
+Enter value of a
+Enter value of B
+click on the button get total
+also please print the value of the attribute type of the button GET TOTAL
+   */
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","Driver/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://practice.syntaxtechs.net/basic-first-form-demo.php");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("//input[contains(@placeholder,'Please enter')]")).sendKeys("Tester");
+        driver.findElement(By.xpath("//button[contains(@onclick,'showInput()')]")).click();
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//input[@id='sum1']")).sendKeys("10");
+        driver.findElement(By.xpath("//input[@id='sum2']")).sendKeys("10");
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//button[text()= 'Get Total']")).click();
+        String totalValue = driver.findElement(By.xpath("//span[@id='displayvalue']")).getText();
+
+        System.out.println("Total value of a + b = " + totalValue);
+
+        Thread.sleep(2000);
+        driver.quit();
+
+
+    }
+}
